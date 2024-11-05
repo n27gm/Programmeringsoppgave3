240bfff55923cded2793615038be0833137aa374
184930 <184930@stud.hvl.no>
2024-11-05 19:10:00 +0100

    Oppgave 4 ferdig

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,5 +1,6 @@
 package no.hvl.dat100.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
@@ -8,8 +9,27 @@ import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        PrintWriter writer = null;
+        try {
+            File file = new File(mappe + File.separator + filnavn);
+            writer = new PrintWriter(file);
+            
+            writer.println(samling.getAntall());
+            
+            for (int i = 0; i < samling.getAntall(); i++) {
+                writer.println(samling.getSamling()[i].toString());
+            }
+            
+            return true; 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); 
+            return false; 
+        } finally {
+            if (writer != null) {
+                writer.close(); 
+            }
+        }
+    }
 }
+
